04139afba3e9a95cffb40391fda9f5333a145968
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-02-28 15:26:54 -0800

    Lab 3.1 (Intro to JUnit Testing) Complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,32 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CalculatorTest {
+    private static final double DELTA = 1e-12;
+    private static final Calculator calc = new Calculator();
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @Test
+    public void testAdd() {
+        System.out.println("testAdd");
+//        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("testDivide");
+//        Calculator calc = new Calculator();
+        assertEquals(2.5, calc.divide(5, 2), DELTA);  // expected, actual, delta
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("testIsEven");
+//        Calculator calc = new Calculator();
+        assertTrue(calc.isEven(10));    // condition
+        assertFalse(calc.isEven(11));   // condition
+    }
 }
